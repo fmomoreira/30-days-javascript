0ebc5f6fc8f28906a8ed3c2d561f42d0e55e55f0
felipe.moreira <felipe.moreira@neogrid.com>
2023-09-27 23:28:23 -0300

    add day-2

--- day-2/resposta.js
+++ day-2/resposta.js
@@ -16,6 +16,20 @@ const multiplicaDeDoisNumeros = (numA, numB) =>{
     return resultado
 }
 
+const divideDeDoisNumeros = (numA, numB) =>{
+    const mult = numA / numB
+    const resultado =  `A divisão de ${numA}  / ${numB} é igual a ${mult}`
+    return resultado
+}
+
+const restoDeDoisNumeros = (numA, numB) =>{
+    const mult = numA % numB
+    const resultado =  `O resto de ${numA}  % ${numB} é igual a ${mult}`
+    return resultado
+}
+
 console.log(somaDeDoisNumeros(2,2))
 console.log(subtraiDeDoisNumeros(9, 5))
 console.log(multiplicaDeDoisNumeros(5,2))
+console.log(divideDeDoisNumeros(15,5))
+console.log(restoDeDoisNumeros(23,7))
